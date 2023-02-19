3d90b7aa5cb74e602fe06dcc9504e11c3e4b9d4b
Luan Cordeiro <luam.lc19@gmail.com>
2023-02-19 11:51:30 -0300

    style: add style to main error message component

--- frontend/src/components/MainComponents.ts
+++ frontend/src/components/MainComponents.ts
@@ -13,4 +13,10 @@ export const PageTitle = styled.h1`
 
 export const PageBody = styled.div``;
 
-export const MessageError = styled.div``;
+export const MessageError = styled.div`
+    margin: 10px;
+    padding: 10px;
+    background-color: #FFCACA;
+    color: #FFF;
+    border: 2px solid #F00;
+`;
