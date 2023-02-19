310a2acfe7d02e95852b27dc04794483afb23c2b
Luan Cordeiro <luam.lc19@gmail.com>
2023-02-19 12:38:45 -0300

    feat: create apiFetchGet. Default function to get request.

--- frontend/src/helpers/OlxAPI.ts
+++ frontend/src/helpers/OlxAPI.ts
@@ -1,4 +1,5 @@
 import Cookies from "js-cookie";
+import qs from 'qs';
 
 const BaseUrl = 'http://alunos.b7web.com.br:501';
 
@@ -20,6 +21,22 @@ const apiFetchPost = async (endPoint: string, body: any) => {
         return;
     }
     return response;
+}
+
+const apiFetchGet = async (endPoint: string, body: any = []) => {
+    if (!body.token) {
+        let token = Cookies.get('token');
+        if (token) {
+            body.token = token;
+        }
+    }
+    const res = await fetch(`${BaseUrl + endPoint}?${qs.stringify(body)}`);
+    const json = await res.json();
+    if (json.notallowed) {
+        window.location.href = '/signin';
+        return;
+    }
+    return json;
 }
 
 const OlxAPI = {
