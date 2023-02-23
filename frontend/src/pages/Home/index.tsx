7894701348d0ea6cd116a44d923f368109610928
Luan Cordeiro <luam.lc19@gmail.com>
2023-02-23 19:30:08 -0300

    feat: start home page create form for search products

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,9 +1,39 @@
+import { useEffect, useState } from 'react';
+import { PageContainer } from '../../components/MainComponents';
+import OlxAPI from '../../helpers/OlxAPI';
+import { StatesType } from '../../types';
 import * as C from './styles';
 
 export const Home = () => {
+    const api = OlxAPI;
+
+    const [states, setStates] = useState<StatesType[]>([]);
+
+    useEffect(() => {
+        const fetchStates = async () => {
+            let response = await api.getStates();
+            setStates(response);
+        }
+        fetchStates();
+    }, []);
+
     return (
-        <C.Container>
-            Home page
-        </C.Container>
+        <>
+            <C.SearchArea>
+                <PageContainer>
+                    <C.SearchBox>
+                        <C.Form action="/ads" method='get'>
+                            <input type="text" name="q" placeholder='O que você procura?' />
+                            <select name="state">
+                                {states.map((item, index) => (
+                                    <option key={index} value={item.name}>{item.name}</option>
+                                ))}
+                            </select>
+                            <button>Pesquisar</button>
+                        </C.Form>
+                    </C.SearchBox>
+                </PageContainer>
+            </C.SearchArea>
+        </>
     )
 }
--- frontend/src/pages/Home/styles.ts
+++ frontend/src/pages/Home/styles.ts
@@ -1,3 +1,50 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const SearchArea = styled.div`
+    background-color: #DDD;
+    padding: 20px 0 40px 0;
+`;
+
+export const SearchBox = styled.div`
+    border-radius: 7px;
+    background-color: #9BB83C;
+    padding: 20px 15px;
+`;
+
+export const Form = styled.form`
+    display: flex;
+    
+    input {
+        flex: 1;
+        height: 40px;
+        padding: 10px;
+        margin-right: 20px;
+        border: 0;
+        outline: 0;
+        font-size: 15px;
+        border-radius: 7px;
+    }
+
+    select {
+        width: 100px;
+        border: 0;
+        outline: 0;
+        margin-right: 20px;
+        border-radius: 7px;
+    }
+
+    button {
+        padding: 0px 20px;
+        background-color: #49AEEF;
+        border: 0;
+        outline: 0;
+        color: #FFF;
+        border-radius: 7px;
+        font-size: 15px;
+        cursor: pointer;
+
+        &:hover {
+            background-color: #006FCE;
+        }
+    }
+`;
