c69abe1646d82f383fcc08debc87ff5caa76fffa
Luan Cordeiro <luam.lc19@gmail.com>
2023-03-04 15:07:02 -0300

    feat: list states on serach area of ads

--- frontend/src/pages/Ads/index.tsx
+++ frontend/src/pages/Ads/index.tsx
@@ -1,6 +1,21 @@
+import { useState, useEffect } from "react"
 import { PageContainer } from "../../components/MainComponents";
+import OlxAPI from "../../helpers/OlxAPI";
+import { StatesType } from "../../types";
 import { AdsBody } from "./styles";
 export const Ads = () => {
+    const api = OlxAPI;
+
+    const [states, setStates] = useState<StatesType[]>([]);
+
+    useEffect(() => {
+        const fetchStates = async () => {
+            const sta = await api.getStates();
+            setStates(sta);
+        }
+        fetchStates();
+    }, []);
+
     return (
         <PageContainer>
             <AdsBody>
@@ -10,6 +25,9 @@ export const Ads = () => {
                         <div className="filterName">Estado:</div>
                         <select name="states">
                             <option value="">Todos</option>
+                            {states.map((item, index) => (
+                                <option key={index} value={item._id}>{item.name}</option>
+                            ))}
                         </select>
                         <div className="filterName">Categoria:</div>
                         <ul>
